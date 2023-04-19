c37aa17f9950801e384c423ece667df7b790a23f
Mr.Ma <88231018+xiaomajy@users.noreply.github.com>
2023-04-19 21:37:29 +0800

    Add files via upload

---
+++ assets/agecalc-c6d3609f.js
@@ -0,0 +1,1 @@
+import{_ as c,c as n,b as o,o as t,f as a}from"./index-13c402c6.js";const r={};function s(_,m){const e=a("mymenu");return t(),n("div",null,[o(e)])}const f=c(r,[["render",s]]);export{f as default};
