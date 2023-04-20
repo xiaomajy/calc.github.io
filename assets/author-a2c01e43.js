0f7a21c545a2edf80ebb5507a34f5a2d55739a99
Mr.Ma <88231018+xiaomajy@users.noreply.github.com>
2023-04-20 23:13:14 +0800

    Add files via upload

---
+++ assets/author-a2c01e43.js
@@ -0,0 +1,1 @@
+import{_ as o,c as n,b as c,o as t,f as r}from"./index-90a0a9f2.js";const s={};function a(_,m){const e=r("mymenu");return t(),n("div",null,[c(e)])}const l=o(s,[["render",a]]);export{l as default};
