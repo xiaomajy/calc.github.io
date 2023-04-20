89fadc5efdd0018f083f093420b2e232f084552e
Mr.Ma <88231018+xiaomajy@users.noreply.github.com>
2023-04-21 01:15:08 +0800

    Delete assets directory

--- assets/check-63e11732.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,a as n,o,c as t,e as r}from"./index-c0eacc8e.js";const s={};function a(_,m){const e=n("mymenu");return o(),t("div",null,[r(e)])}const f=c(s,[["render",a]]);export{f as default};
