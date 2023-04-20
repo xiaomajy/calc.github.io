df68f756a98986d3c0ebcb4fe34455f51d0123ff
Mr.Ma <88231018+xiaomajy@users.noreply.github.com>
2023-04-20 23:09:18 +0800

    Delete assets directory

--- assets/agecalc-1d5a039b.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as n,b as o,o as t,f as a}from"./index-64756679.js";const r={};function s(_,m){const e=a("mymenu");return t(),n("div",null,[o(e)])}const f=c(r,[["render",s]]);export{f as default};
