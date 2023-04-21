b176a77c8fe6cdf7f223dc1d4ad14afceed74684
Mr.Ma <88231018+xiaomajy@users.noreply.github.com>
2023-04-21 16:40:48 +0800

    Delete assets directory

--- assets/author-6f7f3591.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as n,b as c,o as t,f as r}from"./index-6283116f.js";const s={};function a(_,m){const e=r("mymenu");return t(),n("div",null,[c(e)])}const l=o(s,[["render",a]]);export{l as default};
